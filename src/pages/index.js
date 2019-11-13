2114a945c50a307885f85da6b76fa2db7462cdc0
Praecor Loth <praecorloth@gmail.com>
2019-11-13 18:26:08 +0100

    version 3.1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React from "react";
 
 import { Layout, SEO } from "./../layout";
@@ -7,13 +6,7 @@ export default () => {
   return (
     <Layout>
       <SEO title="Welcome" />
-      <h1>Brown fox fucked something somewhere</h1>
-      <h2>Brown fox fucked something somewhere</h2>
-      <h3>Brown fox fucked something somewhere</h3>
-      <h4>Brown fox fucked something somewhere</h4>
-      <h5>Brown fox fucked something somewhere</h5>
-      <h6>Brown fox fucked something somewhere</h6>
-      <p>Brown fox fucked something somewhere</p>
+      <h1>Starter</h1>
     </Layout>
   );
 };
